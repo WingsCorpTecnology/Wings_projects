88ee527551117dc1a36bb657b2dd67617095a8dc
SouzaGiovanna <gi.souza03@gmail.com>
2020-11-14 13:35:06 -0300

    mudança de tela - cadastro funcionario

--- Easychool_v5/app/src/main/java/com/cursoandroid/easychool_v4/activity/ConfigPreferenciasActivity.java
+++ Easychool_v5/app/src/main/java/com/cursoandroid/easychool_v4/activity/ConfigPreferenciasActivity.java
@@ -1,7 +1,10 @@
 package com.cursoandroid.easychool_v4.activity;
 
 import android.content.SharedPreferences;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,7 +13,16 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.cursoandroid.easychool_v4.Base64Custom;
 import com.cursoandroid.easychool_v4.R;
+import com.cursoandroid.easychool_v4.config.ConfiguracaoFirebase;
+import com.cursoandroid.easychool_v4.helper.CalcularDistancia;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class ConfigPreferenciasActivity extends AppCompatActivity {
     private Button btnSalvar;
@@ -18,6 +30,11 @@ public class ConfigPreferenciasActivity extends AppCompatActivity {
     private TextView txtCep;
     private SharedPreferences preferences;
     private final String ARQUIVO_PREFERENCIA = "ArquivoPreferencia";
+    private FirebaseAuth autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+    private DatabaseReference firebaseRef = ConfiguracaoFirebase.getFirebaseDatabase();
+    private DatabaseReference usuarioRef;
+    private String emailResponsavel = autenticacao.getCurrentUser().getEmail();
+    private String idResponsavel = Base64Custom.codificarBase64(emailResponsavel);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +46,7 @@ public class ConfigPreferenciasActivity extends AppCompatActivity {
         txtCep = findViewById(R.id.txtCEP);
 
         preferences = getSharedPreferences(ARQUIVO_PREFERENCIA, 0);
+        usuarioRef = firebaseRef.child("ResponsavelAluno").child(idResponsavel);
 
         btnSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,6 +63,8 @@ public class ConfigPreferenciasActivity extends AppCompatActivity {
                     editor.commit();
 
                     txtCep.setText("CEP: " +cep);
+
+                    geocoding();
                 }
             }
         });
@@ -53,11 +73,42 @@ public class ConfigPreferenciasActivity extends AppCompatActivity {
         if(preferences.contains("cep")){
             String cep = preferences.getString("cep", "usuário não definido");
             txtCep.setText("CEP: " +cep);
+
+
         }
         else{
             txtCep.setText("Nenhum CEP salvo");
         }
 
         getSupportActionBar().hide();
+
+
+    }
+
+    public void geocoding(){
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+        try {
+            List<Address> listaEndereco = geocoder.getFromLocationName("Rua Bertolínia, 272 - Jardim Soares, São Paulo - SP", 1);
+            //List<Address> listaEndereco = geocoder.getFromLocation(-23.5495244, -46.40220360000001, 1);
+            List<Address> listaEnderecoTeste = geocoder.getFromLocation(-23.5926719, -46.6647561, 1);
+
+            if(listaEndereco != null && listaEndereco.size() > 0){
+                Address endereco = listaEndereco.get(0);
+                Address endereco2 = listaEnderecoTeste.get(0);
+
+                Log.d("Local", " "+endereco.toString());
+                Log.d("Local", " "+endereco.getLatitude());
+                Log.d("Local", " "+endereco.getLongitude());
+
+                Double distancia = CalcularDistancia.CalcularDistancia(endereco.getLatitude(), endereco.getLongitude(), endereco2.getLatitude(), endereco2.getLongitude());
+                Log.d("Local", " "+distancia);
+
+                usuarioRef.child("latitude").setValue(endereco.getLatitude());
+                usuarioRef.child("longitude").setValue(endereco.getLongitude());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ Easychool_v5/app/src/main/java/com/cursoandroid/easychool_v4/helper/CalcularDistancia.java
@@ -0,0 +1,19 @@
+package com.cursoandroid.easychool_v4.helper;
+
+public class CalcularDistancia {
+    public static double CalcularDistancia(double lat1, double lng1, double lat2, double lng2) {
+        //double earthRadius = 3958.75;//miles
+        double earthRadius = 6371;//kilometers
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLng = Math.toRadians(lng2 - lng1);
+        double sindLat = Math.sin(dLat / 2);
+        double sindLng = Math.sin(dLng / 2);
+        double a = Math.pow(sindLat, 2) + Math.pow(sindLng, 2)
+                * Math.cos(Math.toRadians(lat1))
+                * Math.cos(Math.toRadians(lat2));
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double dist = earthRadius * c;
+
+        return dist;// * 1000; //em metros
+    }
+}
