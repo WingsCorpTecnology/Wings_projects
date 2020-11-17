38de4fe5f6868528e5f2df441e0ff9b70bfa9d11
SouzaGiovanna <gi.souza03@gmail.com>
2020-11-17 09:22:45 -0300

    cadastro de funcionario

---
+++ Easychool-Site/js/funcionario_database.js
@@ -0,0 +1,61 @@
+const funcionario_database = {};
+
+(function () {
+        let funcionario_id = false;
+
+        function new_funcionario(nome, email, cpf) {
+                const funcionario_data = {
+                        email: email,
+                        nome: nome,
+                        cpf: cpf
+                };
+
+                //if (!escola_id)
+                        //escola_id = firebase.database().ref().child('escolas').push().key;
+                
+                //escola_database.new('gustavo', 'gustavo@gmail.com', '03570-120', 'são paulo', 'são paulo', 'parque savoy city', 'monesia', '34', 'a', '767868957896');
+                
+                //../CadastroFunci/cadastroFuncionario.html
+
+
+                // var escola64 = btoa(unescape(encodeURIComponent(escola)));
+
+                //var escola64 = decodeURIComponent(escape(window.atob(escola)));
+                                
+                var emailKey = email;
+                emailKey.toString();
+                                
+                var emailBase64 = btoa(emailKey);
+                
+                console.log(emailBase64);
+                
+                let updates = {};
+                updates['/Funcionario/' +emailBase64] = funcionario_data;
+
+                let funcionario_ref = firebase.database().ref();
+
+                funcionario_ref.update(updates)
+                        .then(function () {
+                                return {
+                                        sucess: true,
+                                        message: 'Funcionario cadastrado'
+                                };
+                        })
+                        .catch(function (error) {
+                                return {
+                                        sucess: false,
+                                        message: 'Falha ao cadastrar o funcionario: ${error.message}'
+                                };
+                        })
+                //window.location.href = "../CadastroFunci/cadastroFuncionario.html";
+
+        }
+
+        function remove_funcionario() {}
+
+        function update_funcionario() {}
+
+        funcionario_database.new = new_funcionario;
+        funcionario_database.remove = remove_funcionario;
+        funcionario_database.update = update_funcionario;
+})()
