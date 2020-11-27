feab8adb13c8d04e8d76560e7209c26135b1c548
SouzaGiovanna <gi.souza03@gmail.com>
2020-11-27 17:03:35 -0300

    Trocando as imagens

--- Easychool_v5/app/src/main/java/com/cursoandroid/easychool_v4/activity/ConfigFiltrosPesquisaDistanciaActivity.java
+++ Easychool_v5/app/src/main/java/com/cursoandroid/easychool_v4/activity/ConfigFiltrosPesquisaDistanciaActivity.java
@@ -36,7 +36,7 @@ public class ConfigFiltrosPesquisaDistanciaActivity extends AppCompatActivity im
                 finish();
             }
         });
-        
+
         getSupportActionBar().hide();
     }
 
