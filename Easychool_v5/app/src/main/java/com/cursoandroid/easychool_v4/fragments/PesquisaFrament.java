f668420d9166544d277b90c56f4deae606cba013
SouzaGiovanna <gi.souza03@gmail.com>
2020-12-01 18:03:23 -0300

    arrumando bug de mostrar dados

--- Easychool_v5/app/src/main/java/com/cursoandroid/easychool_v4/fragments/PesquisaFrament.java
+++ Easychool_v5/app/src/main/java/com/cursoandroid/easychool_v4/fragments/PesquisaFrament.java
@@ -81,6 +81,7 @@ public class PesquisaFrament extends Fragment {
 
         responsavel = new ResponsavelAluno();
 
+        //recuperarEscolas();
         pesquisa();
         recuperarDadosUser();
 
