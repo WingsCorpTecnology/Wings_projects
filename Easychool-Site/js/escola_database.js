856efe06b2ac05c5abe02c3cf2b196d1080c9dac
Cardozo1007 <gustavocardozo180203@gmai.com>
2020-12-08 23:56:31 -0300

    Faltam algumas coisinhas, mas está quase lá

--- Easychool-Site/js/escola_database.js
+++ Easychool-Site/js/escola_database.js
@@ -2,6 +2,62 @@ const escola_database = {};
 
 (function () {
         let escola_id = false;
+        
+        async function new_telefone(telefone, id_escola){
+                const tel_data = {
+                        "Telefone": telefone
+                }
+                
+                var id_tel = firebase.database().ref().child('TelefoneEscola').push().key;
+                
+                let updates = {};
+                updates['/TelefoneEscola/' + id_escola + '/' + id_tel + '/'] = tel_data;
+                
+                let telefone_ref = firebase.database().ref();
+                
+                await telefone_ref.update(updates)
+                        .then(function(){
+                                return{
+                                        sucess: true
+                                }
+                        })
+                        .catch(function(error){
+                                return{
+                                        sucess: false
+                                }
+                        })
+        }
+        
+        async function info_escola(infantil, fund_1, fund_2, medio, especial, eja, instituicao, id_escola){
+                const nivel_data = {
+                        "Educação Infantil": infantil,
+                        "Ensino Fundamental I": fund_1,
+                        "Ensino Fundamental II": fund_2,
+                        "Ensino Médio": medio,
+                        "Educação Especial": especial,
+                        "EJA": eja,
+                        "Tipo de Instituição": instituicao
+                }
+                
+                let updates = {};
+                updates['/Escola/' + id_escola + '/Filtros/'] = nivel_data;
+                
+                let info_escola_ref = firebase.database().ref();
+                
+                await info_escola_ref.update(updates)
+                        .then(function(){
+                                return{
+                                        sucess: true,
+                                        message: 'ok'
+                                }
+                        })
+                        .catch(function(error){
+                                return{
+                                        sucess: false,
+                                        message: '${error.message}'
+                                }
+                        })
+        }
 
         async function new_escola(nome, email, cep, estado, cidade, bairro, rua, numero, complemento, cnpj) {
                 const escola_data = {
@@ -65,4 +121,6 @@ const escola_database = {};
         escola_database.new = new_escola;
         escola_database.remove = remove_escola;
         escola_database.update = update_escola;
+        escola_database.nivel = info_escola;
+        escola_database.telefone = new_telefone;
 })()
